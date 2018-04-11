611885b3c557c02ddedc78d9fcc0b0e0a9738ef0
Krzysztof Kacprzak <k.kacprzak@oberthur.com>
2018-04-11 11:23:19 +0200

    Pretty output info

--- modules/Memo.js
+++ modules/Memo.js
@@ -182,9 +182,9 @@ let registerEvents = _.once(function (that) {
 
             case 'memo-info': {
                 let id = _.first(args);
-                let memo = that.get(id)
-                if (memo) {
-                    reply.call(that, data, `Memo ${memo.name} metadata: ${_(memo).omitBy((v, k) => ['id', 'name', 'content', 'contents', 'previous', 'next'].indexOf(k) !== -1).map((v, k) => `${_.startCase(k)}: ${v}`).join(', ')}`)
+                let obj = that.get(id)
+                if (obj) {
+                    reply.call(that, data, `Memo ${obj.name} metadata - ${_(obj).omitBy((v, k) => ['id', 'name', 'content', 'contents', 'previous', 'next'].indexOf(k) !== -1).map((v, k) => `${_.lowerCase(k)}: ${v}`).join(', ')}`)
                 } else if (id) {
                     reply.call(that, data, `Memo ${id} not found`);
                 } else {
